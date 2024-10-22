16c8ed94c85778836e5f71734c119560d8adfbaa
srinivas-elimilla <srinivaselimilla1@gmail.com>
2024-10-23 01:11:23 +0530

    prod server url

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const baseUrl = " http://localhost:3000";
+
+const baseUrl = process.env.PROD_URL || "http://localhost:3000";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
